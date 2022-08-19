24937dff6a487db216b42d08e4b106e54edbdd28
Bruno Santos <bruno.lgac.santos@gmail.com>
2022-08-19 14:26:56 +0100

    Created the Directory Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Directory } from "./components/directory/directory.component";
+
+import categories from "./data/categories.json";
+
+const App = () => {
+  return <Directory categories={categories} />;
+};
 
 export default App;
